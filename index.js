2259999d2d2951cfccec28515c59d1c56e47d7d0
dev1 <martiyosrc@gmail.com>
2021-04-02 11:38:47 -0300

    validation

--- index.js
+++ index.js
@@ -25,71 +25,87 @@ app.get("/api/persons", (request, response) => {
 });
 
 app.get("/info", (request, response) => {
-  response.send(
-    `<p>Phonebook has info for ${persons.length} people</p> ${Date()}`
-  );
-});
-
-app.get("/api/persons/:id", (request, response) => {
-  Person.findById(request.params.id).then((person) => {
-    response.json(person);
-    //(response.status(404).end());
+  Person.find({}).then((persons) => {
+    response.send(
+      `<p>Phonebook has info for ${persons.length} people</p> ${Date()}`
+    );
   });
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
-  response.status(204).end();
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
-const generateId = () => {
-  return Math.random();
-};
+app.delete("/api/persons/:id", (request, response) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const body = request.body;
+  if (body.name === undefined) {
+    return response.status(400).json({ error: "content missing" });
+  }
 
   const person = new Person({
     name: body.name,
     number: body.number,
   });
 
-  person.save().then((savedPerson) => {
-    response.json(savedPerson);
-  });
-
-  /* if (!body.name || !body.number) {
-     *   return response.status(400).json({
-     *     error: "name and number are missing",
-     *   });
-     * }
-
-     * const result = persons.find((person) => person.name === body.name);
-     * console.log(result);
-     * if (result) {
-     *   return response.status(400).json({
-     *     error: "name must be unique",
-     *   });
-     * }
-
-     * const person = {
-     *   name: body.name,
-     *   number: body.number,
-     *   date: new Date(),
-     *   id: generateId(),
-     * };
-
-     * persons = persons.concat(person);
-
-     * response.status(201).json(persons); */
+  person
+    .save()
+    .then((savedPerson) => savedPerson.toJSON())
+    .then((savedAndFormattedPerson) => {
+      response.json(savedAndFormattedPerson);
+    })
+    .catch((error) => next(error));
 });
 
 const unknowEndpoint = (request, response) => {
   response.status(404).send({ error: "unknow endpoint" });
 };
 
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body;
+
+  const note = {
+    name: body.name,
+    number: body.number,
+  };
+
+  Person.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then((updatedNote) => {
+      response.json(updatedNote);
+    })
+    .catch((error) => next(error));
+});
+
 app.use(unknowEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
--- models/person.js
+++ models/person.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
 
 const url = process.env.MONGODB_URI;
 
@@ -19,8 +20,16 @@ mongoose
   });
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 3,
+  },
+  number: {
+    type: String,
+    minlength: 8,
+  },
 });
 
 personSchema.set("toJSON", {
@@ -30,5 +39,7 @@ personSchema.set("toJSON", {
     delete returnedObject.__v;
   },
 });
+
+personSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model("Person", personSchema);
